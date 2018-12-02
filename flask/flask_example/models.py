5cbfeacb3f26e71a2bbd292a1549286dfd1385ed
xuchu <mikasa4869@163.com>
2018-12-02 18:12:42 +0800

    save

--- flask/flask_example/models.py
+++ flask/flask_example/models.py
@@ -1,5 +1,9 @@
 from flask_example import db
 
+Column = db.Column
+Integer = db.Integer
+String = db.String
+
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
@@ -11,3 +15,33 @@ class Student(db.Model):
     def __repr__(self):
 
         return '<User %r>' % self.name
+
+class Cource(db.Model):
+    __tablename__ = 'cource'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(80), nullable=False)
+    teacher_id = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Cource(name=%s)>' % self.name
+
+
+class Teacher(db.Model):
+    __tablename__ = 'teacher'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(80), nullable=False)
+
+    def __repr__(self):
+        return '<Teacher(name=%s)>' % self.name
+
+
+class Score(db.Model):
+    __tablename__ = 'score'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    student_id = Column(Integer, nullable=False)
+    cource_id = Column(Integer, nullable=False)
+    score = Column(Integer, default=0, nullable=False)
+
+    def __repr__(self):
+        return '<Score()>'
