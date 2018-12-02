f13d162ccf647770f62531510012ff8c951e9de0
xuchu <mikasa4869@163.com>
2018-12-02 18:04:00 +0800

    save

--- flask/flask_example/__init__.py
+++ flask/flask_example/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+from flask import Response
+
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('flask_example.config')
 print(app.config['SQLALCHEMY_DATABASE_URI'])
@@ -11,8 +13,14 @@ from .models import Student
 
 @app.route('/')
 def index():
+    # s = Student(name='lisongsong')
+    # session = Student.query.session
+    # session.add(s)
+    # session.commit()
+
     students = Student.query.all()
     records = []
     for student in students:
-        records.append(student.__repr__())
-    return json.dumps(records)
+        records.append(student.to_string())
+    data = {'records': records}
+    return Response(response=json.dumps(data))
--- flask/flask_example/models.py
+++ flask/flask_example/models.py
@@ -1,8 +1,13 @@
 from flask_example import db
 
 class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
 
+    def to_string(self):
+        # return '<User %r>' % self.name
+        return {'name': self.name}
+
     def __repr__(self):
+
         return '<User %r>' % self.name
