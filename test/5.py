125ddf02d39c25e2da577b74fa34bc5622199c0b
李松松 <songsong.li@cootek.cn>
2018-12-07 10:33:58 +0800

    rename

--- test/5.py
+++
@@ -1,8 +0,0 @@
-
-select *.*
- from reportfield join  reportextinfo
-   on reportextinfo.flow_id=reportfield.flow_id
-join mart_poi.dp_mt_poi mtdp
-   on mtdp.dt='20181115' and mtdp.dp_shop_id=reportfield.dp_id
- left join dpshop
-   on dpshop.shop_id=mtdp.dp_
