cf93d0ea05888272d868861703c6e2d0b711ed36
李松松 <songsong.li@cootek.cn>
2019-05-14 14:27:14 +0800

    test commit

---
+++ flask/flask_example/xx.py
@@ -0,0 +1,10 @@
+from git import Repo
+
+repo = Repo('/Users/songsong/Documents/program/py3test')
+git = repo.git
+
+print(git.diff(name_only=True))
+print(repo.untracked_files)
+
+
+print(git.status())
